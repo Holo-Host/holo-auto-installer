97f947a5519bc26fb1c90f6216d2ec5a0a589cd2
peeech <pj@imagine-nyc.com>
2022-11-08 12:07:54 +0100

    add logging

--- src/entries.rs
+++ src/entries.rs
@@ -6,6 +6,7 @@ use serde::Deserialize;
 use serde::Serialize;
 use std::fs::File;
 use std::{collections::HashMap, env};
+use tracing::info;
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct DnaResource {
@@ -39,6 +40,7 @@ impl Preferences {
     /// which allows hpos-holochain-api to read current values
     pub fn save(self) -> Result<Self> {
         if let Ok(path) = env::var("SL_PREFS_PATH") {
+            info!("Writing default servicelogger prefs to {}", &path);
             // create or overwrite to a file
             let file = File::create(&path)?;
             serde_yaml::to_writer(file, &self).context(format!(
