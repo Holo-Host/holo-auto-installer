4ffaf74afde1a16daca08fd0c9c02dbd9f15a101
zo-el <joelulahanna@gmail.com>
2023-09-28 14:38:37 -0500

    holochain: v0.2.2

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Template for Holochain app development";
+
+  inputs = {
+    versions.url = "github:holochain/holochain?dir=versions/0_2";
+ 
+    holochain-flake.url = "github:holochain/holochain";
+    holochain-flake.inputs.versions.follows = "versions";
+
+    nixpkgs.follows = "holochain-flake/nixpkgs";
+    flake-parts.follows = "holochain-flake/flake-parts";
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; }
+      {
+        systems = builtins.attrNames inputs.holochain-flake.devShells;
+
+        perSystem =
+          { inputs'
+          , config
+          , pkgs
+          , system
+          , ...
+          }: {
+
+            devShells.default = pkgs.mkShell {
+              inputsFrom = [ inputs'.holochain-flake.devShells.holonix ];
+              packages = [
+                pkgs.nodejs-18_x
+                pkgs.binaryen
+                # more packages go here
+              ];
+            };
+          };
+      };
+}
