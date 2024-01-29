c83f6aa2e41d4b4767983d53ab4f0b47207a6ff8
Zeeshan <zeeshan.abid.com@gmail.com>
2024-01-29 12:28:28 +0000

    feat: updated lib

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ pub mod websocket;
 use anyhow::Result;
 pub use websocket::{AdminWebsocket, AppWebsocket};
 pub mod host_zome_calls;
+pub mod transaction_types;
 use host_zome_calls::get_all_published_hosted_happs;
 mod install_app;
 use install_app::install_holo_hosted_happs;
@@ -14,8 +15,10 @@ use tracing::{debug, info};
 use uninstall_apps::uninstall_ineligible_happs;
 mod get_kyc_level;
 use get_kyc_level::{get_kyc_level, KycLevel};
+mod suspend_happs;
+use suspend_happs::suspend_unpaid_happs;
 
-use crate::host_zome_calls::CoreAppClient;
+use crate::host_zome_calls::{get_pending_transactions, CoreAppClient};
 
 /// gets all the enabled happs from HHA
 /// installs and enables new happs that were registered by a provider and holochain disables those paused by provider in hha
@@ -28,6 +31,8 @@ pub async fn run(core_happ: &config::Happ, config: &config::Config) -> Result<()
 
     let mut core_app_client = CoreAppClient::connect(core_happ, config).await?;
     let list_of_happs = get_all_published_hosted_happs(&mut core_app_client).await?;
+    let pending_transactions = get_pending_transactions(&mut core_app_client).await?;
+    suspend_unpaid_happs(&list_of_happs, pending_transactions).await?;
     install_holo_hosted_happs(config, &list_of_happs, is_kyc_level_2).await?;
     uninstall_ineligible_happs(config, &list_of_happs, is_kyc_level_2).await?;
     Ok(())
